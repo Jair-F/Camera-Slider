d2189e00122c4d0f9e3beb4671b5ef7e3df6c8db
Jair <jair.fehlauer@gmail.com>
2021-09-10 15:38:56 +0300

    inital commit

---
+++ include/CameraSlider.hpp
@@ -0,0 +1,76 @@
+#include <AccelStepper.h>
+
+#pragma once
+
+struct Position {
+	long x, y, z;
+};
+
+class CameraSlider {
+public:
+	CameraSlider(uint8_t x_axis_pin_1, uint8_t x_axis_pin_2, uint8_t x_axis_pin_3, uint8_t x_axis_pin_4, 
+				 uint8_t y_axis_pin_1, uint8_t y_axis_pin_2, uint8_t y_axis_pin_3, uint8_t y_axis_pin_4, 
+				 uint8_t z_axis_pin_1, uint8_t z_axis_pin_2, uint8_t z_axis_pin_3, uint8_t z_axis_pin_4):	x_axis(x_axis_pin_1, x_axis_pin_2, x_axis_pin_3, x_axis_pin_4),
+				 																							y_axis(y_axis_pin_1, y_axis_pin_2, y_axis_pin_3, y_axis_pin_4),
+				 																							z_axis(z_axis_pin_1, z_axis_pin_2, z_axis_pin_3, z_axis_pin_4) { }
+
+	Position getCurrentPosition();
+
+	void run();
+
+	void setSpeed(float speed);
+
+	void setAcceleration(float acceleration);
+
+protected:
+	/*
+		Calculates the speed for every axis according to their distance they have to go, that all the axis need the same time to travel different lenth of wayss
+	*/
+	void calcAndSetNewSpeed(float speed);
+
+private:
+	AccelStepper x_axis;
+	AccelStepper y_axis;
+	AccelStepper z_axis;
+
+	
+};
+
+
+
+// Implementation
+
+Position CameraSlider::getCurrentPosition() {
+	Position ret;
+	ret.x = x_axis.currentPosition();
+	ret.y = y_axis.currentPosition();
+	ret.z = z_axis.currentPosition();
+	return ret;
+}
+
+void CameraSlider::calcAndSetNewSpeed(float speed) {
+	float one_precent_of_speed = speed / 100;
+	/*
+		find greatest distance, because we need to set the speed accordingly(if we have a shorter way to go we need to reduce the speed)
+		The greatest distance has 100% of the speed. The others are changed to the precent of their range(greatest_distance * 100 / other_distance)
+	*/
+	long greatest_distance = x_axis.distanceToGo();
+	if(y_axis.distanceToGo() > greatest_distance) {
+		greatest_distance = y_axis.distanceToGo();
+	} 
+	else if (y_axis.distanceToGo() > x_axis.distanceToGo()) {
+		greatest_distance = y_axis.distanceToGo();
+	}
+
+	float one_percent_of_greatest_distance = greatest_distance / 100.0;	// Convert the 100 to a float(100.0) in order to not cut the numbers after the comma
+
+	x_axis.setSpeed(x_axis.distanceToGo() / one_percent_of_greatest_distance * one_precent_of_speed);
+	y_axis.setSpeed(y_axis.distanceToGo() / one_percent_of_greatest_distance * one_precent_of_speed);
+	z_axis.setSpeed(z_axis.distanceToGo() / one_percent_of_greatest_distance * one_precent_of_speed);
+}
+
+void CameraSlider::run() {
+	x_axis.run();
+	y_axis.run();
+	z_axis.run();
+}
