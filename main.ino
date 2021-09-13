abf27b5d9d2d070253f198181d6f83f36564a836
Jair <jair.fehlauer@gmail.com>
2021-09-13 18:00:53 +0300

    Removed wrong motor pins

--- main.ino
+++ main.ino
@@ -16,15 +16,9 @@
 #define MOTOR_3_PIN_3 3
 #define MOTOR_3_PIN_4 4
 
-#define MOTOR_4_PIN_1 1
-#define MOTOR_4_PIN_2 2
-#define MOTOR_4_PIN_3 3
-#define MOTOR_4_PIN_4 4
-
 CameraSlider slider(MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_PIN_3, MOTOR_1_PIN_4, 
 					MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_PIN_3, MOTOR_2_PIN_4, 
-					MOTOR_3_PIN_1, MOTOR_3_PIN_2, MOTOR_3_PIN_3, MOTOR_3_PIN_4, 
-					MOTOR_4_PIN_1, MOTOR_4_PIN_2, MOTOR_4_PIN_3, MOTOR_4_PIN_4);
+					MOTOR_3_PIN_1, MOTOR_3_PIN_2, MOTOR_3_PIN_3, MOTOR_3_PIN_4);
 
 void setup() {
 	Serial.begin(9600);
