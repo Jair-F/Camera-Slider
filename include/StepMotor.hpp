1698333e744c6d8d63266d0c145be0f0ca3b8a46
Jair <jair.fehlauer@gmail.com>
2021-09-17 15:11:53 +0300

    moved lastStepSpeed in relation to distanceTraveld

--- include/StepMotor.hpp
+++ include/StepMotor.hpp
@@ -30,9 +30,11 @@ public:
 	void setDistanceTraveld(long long _distance_traveld) { distance_traveld = _distance_traveld; }
 
 	/*
-		set the last step speed and calculate from it the new acceleration to the already given target_position
+		set the last step speed and calculate from it the new acceleration to the position of the traveld distance
+		@param lastStepSpeed the speed, the motor will turn on the last step to the traveldDistancePosition
+		@param traveldDistancePosition The speed will de-/accelerate until the motor reaches this point of made steps(doesnt depends in which direction the motor turned)
 	*/
-	void setLastStepSpeed(double lastStepSpeed);
+	void setLastStepSpeed(double lastStepSpeed, long long traveldDistancePosition);
 
 	/*
 		@param _maxSpeed in this case the higher the value is the faster the motor will move(figure out the value for your motor)
@@ -47,11 +49,12 @@ public:
 		@param _lastStepSpeed The motor will incerase his speed from the previous set speed to this speed(_lastStepSpeed) in such a way
 		that he incerases the speed constantly at every step and this value is the speed of the las step
 	*/
-	void moveTo(long long pos, double _lastStepSpeed);
+	void moveTo(long long pos);
 
 	/*
 		Goes only one step forward with the given speed
 		@param direction true for clockwise
+		@param speed the delay in milliseconds between the steps
 	*/
 	void move(bool direction, double speed);
 
@@ -82,7 +85,7 @@ protected:
 		The metiont parameters need to be already set to the new parameters
 	*/
 	void set_new_speed_incerase_per_step() {
-		speed_change_per_step = (abs(target_last_step_speed - current_speed)) / (abs(target_position - current_position));
+		speed_change_per_step = (abs(target_last_step_speed - current_speed)) / (abs(target_distance_traveld_position - distance_traveld));
 
 		// If we need to decerase the speed
 		if(target_last_step_speed < current_speed) {
@@ -97,10 +100,12 @@ private:
 
 	long long current_position;	// the position(if we are on position 0 and go then 10 to plus and afterwards 10 to minus the position os again 0)
 	long long target_position;
-	long long distance_traveld;	// if we are on position 0 and go then 10 to plus and afterwards 10 to minus the distance we traveld is 20
+
+	long long distance_traveld;					// if we are on position 0 and go then 10 to plus and afterwards 10 to minus the distance we traveld is 20
+	long long target_distance_traveld_position;	// stores the point, until where the speed will incerase to the target_last_step_speed
 	
 	double current_speed;				// The actual Speed. speed is the of milliseconds number, we delay between the single steps!
-	double target_last_step_speed;	// The speed of the last step should be this speed
+	double target_last_step_speed;	// The speed of the last(of the target_distance_traveld_position) step should be this speed
 	double speed_change_per_step;	// In order to reach the target_last_step_speed we need to incerase the speed of each step with this value
 
 	long long last_step_time_point = 0;
@@ -108,16 +113,14 @@ private:
 
 // Implementations
 
-void StepMotor::setLastStepSpeed(double _lastStepSpeed) {
+void StepMotor::setLastStepSpeed(double _lastStepSpeed, long long traveldDistancePosition) {
 	target_last_step_speed = _lastStepSpeed;
+	target_distance_traveld_position = traveldDistancePosition;
 	set_new_speed_incerase_per_step();
 }
 
-void StepMotor::moveTo(long long pos, double _lastStepSpeed) {
+void StepMotor::moveTo(long long pos) {
 	target_position = pos;
-	target_last_step_speed = _lastStepSpeed;
-
-	set_new_speed_incerase_per_step();
 }
 
 void StepMotor::move(bool direction, double speed) {
@@ -135,6 +138,8 @@ void StepMotor::move(bool direction, double speed) {
 	long timer_end = millis();
 	
 	++distance_traveld;
+	current_speed = speed;
+	set_new_speed_incerase_per_step();	// In case we will afterwards move to the target position we store the speed and clac the new de-/acceleration
 
 	long difference = timer_end - timer_begin;
 	if(speed - difference > 0) {
